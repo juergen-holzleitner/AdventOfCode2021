5a29b0f35e945b784097a67aaba350600ba28436
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-01 12:20:36 +0100

    count number of increases

--- 1-SonarSweep/Program.cs
+++ 1-SonarSweep/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var input = File.ReadAllLines("Input.txt");
+int previousValue = int.MaxValue;
+int numIncreases = 0;
+foreach (var line in input)
+{
+  int currentValue = int.Parse(line.Trim());
+  if (currentValue > previousValue)
+  {
+    ++numIncreases;
+  }
+  previousValue = currentValue;
+}
+
+Console.WriteLine("Increases: " + numIncreases);
