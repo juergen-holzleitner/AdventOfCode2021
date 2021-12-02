ce324f22df66d6731375b83904ae64ef679ecb4e
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-02 22:42:17 +0100

    add part one of Dive

--- 2-Dive/Program.cs
+++ 2-Dive/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = File.ReadLines(@"Input.txt");
+int horizontalPosition = 0;
+int depth = 0;
+foreach (var line in input)
+{
+  var tokens = line.Split(' ');
+  
+  // skip empty lines
+  if (tokens.Length < 2)
+    continue;
+
+  int value = int.Parse(tokens[1]);
+  switch (tokens[0].Trim())
+  {
+    case "forward":
+      horizontalPosition += value;
+      break;
+    case "down":
+      depth += value;
+      break;
+    case "up":
+      depth -= value;
+      break;
+    default:
+      System.Diagnostics.Debug.Assert(false, $"Invalid token {tokens[0]}");
+      break;
+
+  }
+}
+Console.WriteLine(horizontalPosition * depth);
