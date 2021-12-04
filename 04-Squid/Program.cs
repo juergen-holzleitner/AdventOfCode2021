8e64b4ec5e87ee9e4c12e91dd800da03146340c4
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-04 12:50:48 +0100

    read all bingo cards

--- 04-Squid/Program.cs
+++ 04-Squid/Program.cs
@@ -7,34 +7,72 @@ Console.WriteLine("Hello, World!");
 BingoGame ReadFileData(string fileName)
 {
   var fileData = File.ReadLines(fileName);
-  var bingoGame = new BingoGame();
 
   var enumerator = fileData.GetEnumerator();
   if (!enumerator.MoveNext())
-    return bingoGame;
+    throw new ApplicationException("no data available");
 
   var firstLine = enumerator.Current;
+  var drawnNumbers = from n in firstLine.Split(',') select int.Parse(n);
+  var bingoGame = new BingoGame(drawnNumbers);
 
-  bingoGame.drawnNumbers = from n in firstLine.Split(',') select int.Parse(n);
-
-
+  BingoCard? currentCard = null;
   while (enumerator.MoveNext())
-  { 
-  
+  {
+    string line = enumerator.Current;
+    if (string.IsNullOrEmpty(line))
+    {
+      AddCardToGame(currentCard, bingoGame);
+      currentCard = new BingoCard();
+      continue;
+    }
+
+    System.Diagnostics.Debug.Assert(currentCard != null);
+
+    var row = (from n in line.Split(' ', StringSplitOptions.RemoveEmptyEntries) select int.Parse(n)).ToList();
+    currentCard.bingoPositions.Add(row);
   }
+  AddCardToGame(currentCard, bingoGame);
 
   return bingoGame;
 }
 
+void AddCardToGame(BingoCard? currentCard, BingoGame bingoGame)
+{
+  if (currentCard != null)
+  {
+    System.Diagnostics.Debug.Assert(currentCard.bingoPositions.Count == 5);
+    foreach (var row in currentCard.bingoPositions)
+    {
+      System.Diagnostics.Debug.Assert(row.Count == 5);
+      foreach (var number in row)
+        System.Diagnostics.Debug.Assert(number >= 0);
+    }
+
+    bingoGame.bingoCards.Add(currentCard);
+  }
+}
+
 struct BingoPosition
 {
-  public int Number { get; set; }
+  public int Number { get; set; } = -1;
   public bool IsHit { get; set; } = false;
+}
+
+class BingoCard
+{
+  public List<List<int>> bingoPositions = new List<List<int>>();
 }
 
 struct BingoGame
 {
+  public BingoGame(IEnumerable<int> drawnNumbers)
+  {
+    this.drawnNumbers = drawnNumbers;
+  }
+
   public IEnumerable<int> drawnNumbers;
+  public List<BingoCard> bingoCards = new List<BingoCard>();
 }
 
 
