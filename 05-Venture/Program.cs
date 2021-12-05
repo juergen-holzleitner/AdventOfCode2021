c69dd23376ca5b20df837469f54eb6a4c1ac801a
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-05 13:10:58 +0100

    parse input

--- 05-Venture/Program.cs
+++ 05-Venture/Program.cs
@@ -1,14 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 var data = ReadInput(@"input-small.txt");
+foreach (var item in data.Lines)
+{
+  Console.WriteLine(item);
+}
 
 Input ReadInput(string fileName)
 {
   var data = File.ReadLines(fileName);
   var input = new Input();
+  var regEx = new System.Text.RegularExpressions.Regex(@"(?<x1>\d),(?<y1>\d) -> (?<x2>\d),(?<y2>\d)");
   foreach (var line in data)
   {
-    Console.WriteLine(line);
+    var groups = regEx.Match(line).Groups;
+
+    var p1 = new Point(int.Parse(groups["x1"].Value), int.Parse(groups["y1"].Value));
+    var p2 = new Point(int.Parse(groups["x2"].Value), int.Parse(groups["y2"].Value));
+
+    if (p1.x == p2.x || p1.y == p2.y)
+    {
+      input.Lines.Add(new Line(p1, p2));
+    }
   }
 
   return input;
@@ -16,6 +29,9 @@ Input ReadInput(string fileName)
 
 class Input
 {
-
+  public List<Line> Lines { get; set; } = new();
 }
 
+readonly record struct Point (int x, int y);
+
+readonly record struct Line (Point start, Point end);
