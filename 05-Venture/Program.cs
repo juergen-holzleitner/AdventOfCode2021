3cfd143c3b1a11e678ed4bbd5002ad5d0c8364dc
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-05 12:20:16 +0100

    read file input

--- 05-Venture/Program.cs
+++ 05-Venture/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+var data = ReadInput(@"input-small.txt");
+
+Input ReadInput(string fileName)
+{
+  var data = File.ReadLines(fileName);
+  var input = new Input();
+  foreach (var line in data)
+  {
+    Console.WriteLine(line);
+  }
+
+  return input;
+}
+
+class Input
+{
+
+}
+
