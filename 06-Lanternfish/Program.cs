5084a60ea5862ecf4b40244a150c8c8a2494958b
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-06 21:41:50 +0100

    part 1 of Lanternfish

--- 06-Lanternfish/Program.cs
+++ 06-Lanternfish/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = GetInput(@"input.txt");
+
+for (int i = 0; i < 80; ++i)
+{
+  input = ProcessDay(input);
+  // Console.WriteLine($"{i}: {String.Join(',', input)}");
+}
+
+Console.WriteLine(input.Count);
+
+List<int> GetInput(string fileName)
+{
+  var s = File.ReadAllText(fileName);
+  var data = from n in s.Split(',') select int.Parse(n);
+  return data.ToList();
+}
+
+List<int> ProcessDay(List<int> current)
+{
+  List<int> newDay = new();
+
+  int itemsToAdd = 0;
+  foreach (var n in current)
+  {
+    if (n == 0)
+    {
+      ++itemsToAdd;
+      newDay.Add(6);
+    }
+    else
+    {
+      newDay.Add(n - 1);
+    }
+  }
+
+  for (int n = 0; n < itemsToAdd; ++n)
+    newDay.Add(8);
+
+  return newDay;
+}
