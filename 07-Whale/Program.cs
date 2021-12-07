40ccef93c8c9f61fcde8f99129b25eb5382460c4
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-07 20:25:12 +0100

    part 1 of 07-Whale

--- 07-Whale/Program.cs
+++ 07-Whale/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = GetInput(@"input.txt");
+
+int? minNeededFuel = null;
+for (int n = input.Min(); n <= input.Max(); n++)
+{
+  int neededFuel = GetNeededFuel(input, n);
+  // Console.WriteLine($"{n}: {neededFuel}");
+  if (!minNeededFuel.HasValue || neededFuel < minNeededFuel)
+    minNeededFuel = neededFuel;
+}
+
+Console.WriteLine(minNeededFuel);
+
+int GetNeededFuel(int[] input, int n)
+{
+  int fuel = 0;
+  foreach (var i in input)
+  {
+    fuel += Math.Abs(n - i);
+  }
+  return fuel;
+}
+
+int[] GetInput(string fileName)
+{
+  var data = File.ReadAllText(fileName);
+  var values = (from n in data.Split(',') select int.Parse(n)).ToArray();
+  return values;
+}
