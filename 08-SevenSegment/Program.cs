6e7a6ea0131cb3469c8eb897b339f50db451a4fa
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-08 18:04:29 +0100

    add 08-SevenSegment

---
+++ 08-SevenSegment/Program.cs
@@ -0,0 +1,23 @@
+﻿var input = GetInput(@"input-small.txt");
+
+Console.WriteLine();
+
+IEnumerable<Input> GetInput(string fileName)
+{
+  var fileData = File.ReadLines(fileName);
+  var regEx = new System.Text.RegularExpressions.Regex(@"(?<pattern>.+) \| (?<output>.+)");
+  foreach (var line in fileData)
+  {
+    var match = regEx.Match(line);
+    if (match.Success)
+    {
+      var pattern = match.Groups["pattern"].Value;
+      var output = match.Groups["output"].Value;
+
+
+      yield return new Input(pattern.Split(' '), output.Split(' '));
+    }
+  }
+}
+
+readonly record struct Input(IEnumerable<string> Patterns, IEnumerable<string> Outputs);
