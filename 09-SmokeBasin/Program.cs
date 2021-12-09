0fc90173100effd5d53f69683f7c4fca2eef966a
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-09 18:36:32 +0100

    add 09-SmokeBasin

---
+++ 09-SmokeBasin/Program.cs
@@ -0,0 +1,16 @@
+﻿var input = GetInput(@"input-small.txt");
+
+Console.WriteLine();
+
+List<List<int>> GetInput(string fileName)
+{
+  var lines = File.ReadLines(fileName);
+  List<List<int>> input = new();
+
+  foreach (var line in lines)
+  {
+    var row = (from x in line select int.Parse(x.ToString())).ToList();
+    input.Add(row);
+  }
+  return input;
+}
