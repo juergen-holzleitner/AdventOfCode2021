b60bb7a8e203e664d20df4ef2c55a78a89250dc5
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-10 16:07:37 +0100

    part2 of 10-Syntax

--- 10-Syntax/Program.cs
+++ 10-Syntax/Program.cs
@@ -16,17 +16,72 @@ Dictionary<char, int> errorScore = new()
   { '>', 25137 },
 };
 
-int totalScore = 0;
+Dictionary<char, int> closingScore = new()
+{
+  { ')', 1 },
+  { ']', 2 },
+  { '}', 3 },
+  { '>', 4 },
+};
+
+//int totalScore = 0;
+//foreach (var line in input)
+//{
+//  var invalidClosing = GetInvalidClosing(line);
+//  if (invalidClosing != null)
+//  {
+//    totalScore += errorScore[invalidClosing.Value];
+//  }
+//}
+
+List<long> closingScores = new();
 foreach (var line in input)
 {
-  var invalidClosing = GetInvalidClosing(line);
-  if (invalidClosing != null)
+  var missingClosings = GetMissingClosings(line);
+  if (!missingClosings.Any())
+    continue;
+
+  long totalClosingScore = 0;
+  foreach (var c in missingClosings)
   {
-    totalScore += errorScore[invalidClosing.Value];
+    totalClosingScore *= 5;
+    totalClosingScore += closingScore[c];
   }
+  if (totalClosingScore > 0)
+    closingScores.Add(totalClosingScore);
 }
 
-Console.WriteLine(totalScore);
+var sortedScores = closingScores.OrderBy(s => s).ToList();
+
+var middleScore = sortedScores[sortedScores.Count() / 2];
+
+Console.WriteLine(middleScore);
+
+List<char> GetMissingClosings(string line)
+{
+  Stack<char> openings = new();
+
+  List<char> missingClosings = new();
+
+  foreach (char c in line)
+  {
+    if (IsOpening(c))
+      openings.Push(c);
+    else
+    {
+      var stacked = openings.Pop();
+      if (c != braceMatches[stacked])
+        return missingClosings;
+    }
+  }
+
+  while (openings.Any())
+  {
+    var closing = braceMatches[openings.Pop()];
+    missingClosings.Add(closing);
+  }
+  return missingClosings;
+}
 
 char? GetInvalidClosing(string line)
 {
