ed7de03cd0186154939cc1a4542a87631209d8bc
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-10 15:44:42 +0100

    part1 of 10-Syntax

---
+++ 10-Syntax/Program.cs
@@ -0,0 +1,60 @@
+﻿var input = GetInput(@"input.txt");
+
+Dictionary<char, char> braceMatches = new()
+{
+  { '(', ')' },
+  { '[', ']' },
+  { '{', '}' },
+  { '<', '>' },
+};
+
+Dictionary<char, int> errorScore = new()
+{
+  { ')', 3 },
+  { ']', 57 },
+  { '}', 1197 },
+  { '>', 25137 },
+};
+
+int totalScore = 0;
+foreach (var line in input)
+{
+  var invalidClosing = GetInvalidClosing(line);
+  if (invalidClosing != null)
+  {
+    totalScore += errorScore[invalidClosing.Value];
+  }
+}
+
+Console.WriteLine(totalScore);
+
+char? GetInvalidClosing(string line)
+{
+  Stack<char> openings = new();
+
+  foreach (char c in line)
+  {
+    if (IsOpening(c))
+      openings.Push(c);
+    else
+    {
+      var stacked = openings.Pop();
+      if (c != braceMatches[stacked])
+        return c;
+    }
+  }
+
+  return null;
+}
+
+IEnumerable<string> GetInput(string fileName)
+{
+  return File.ReadLines(fileName);
+}
+
+
+
+bool IsOpening(char ch)
+{
+  return braceMatches.ContainsKey(ch);
+}
