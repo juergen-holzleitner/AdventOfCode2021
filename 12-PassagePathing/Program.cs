f9b1e42f1429eda66bcc104272269b1bc28cfbc8
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-12 20:17:14 +0100

    read input

--- 12-PassagePathing/Program.cs
+++ 12-PassagePathing/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var connections = GetInput(@"input-small.txt");
+Console.WriteLine();
+
+List<Connection> GetInput(string fileName)
+{
+  List<Connection> connections = new();
+
+  var regExp = new System.Text.RegularExpressions.Regex(@"(?<start>\w+)-(?<end>\w+)");
+  foreach (var line in File.ReadLines(fileName))
+  {
+    var match = regExp.Match(line);
+    if (match.Success)
+    {
+      var start = match.Groups["start"].Value;
+      var end = match.Groups["end"].Value;
+      connections.Add(new Connection(start, end));
+    }
+  }
+
+  return connections;
+}
+
+
+readonly record struct Connection(string start , string end);
