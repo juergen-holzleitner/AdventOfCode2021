1590d12c16844baad07e36a3ccf55f3642124913
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-13 18:18:26 +0100

    part 2 of 13-Origami

--- 13-Origami/Program.cs
+++ 13-Origami/Program.cs
@@ -1,10 +1,59 @@
-﻿var input = GetInput(@"input-small.txt");
-Console.WriteLine(input);
+﻿var input = GetInput(@"input.txt");
+HashSet<Point> points = input.Points;
+foreach (var fold in input.Folds)
+{
+  points = Fold(points, fold);
+}
 
+PrintPoints(points);
+
+
+void PrintPoints(HashSet<Point> points)
+{
+  int maxX = points.Max(p => p.x);
+  int maxY = points.Max(p => p.y);
+
+  for (int y = 0; y <= maxY; y++)
+  {
+    for (int x = 0; x <= maxX; x++)
+    {
+      if (points.Contains(new Point(x, y)))
+        Console.Write('#');
+      else
+        Console.Write(' ');
+    }
+    Console.WriteLine();
+  }
+}
+
+HashSet<Point> Fold(HashSet<Point> input, Point foldPos)
+{
+  var newPoints = new HashSet<Point>();
+  foreach (var point in input)
+  {
+    if (foldPos.x == 0)
+    {
+      System.Diagnostics.Debug.Assert(point.y != foldPos.y);
+      if (point.y < foldPos.y)
+        newPoints.Add(point);
+      else
+        newPoints.Add(point with { y = 2 * foldPos.y - point.y });
+    }
+    else if (foldPos.y == 0)
+    {
+      System.Diagnostics.Debug.Assert(point.x != foldPos.x);
+      if (point.x < foldPos.x)
+        newPoints.Add(point);
+      else
+        newPoints.Add(point with { x = 2 * foldPos.x - point.x });
+    }
+  }
+  return newPoints;
+}
 
 Input GetInput(string fileName)
 {
-  List<Point> points = new();
+  HashSet<Point> points = new();
   List<Point> folds = new();
 
   var regPoints = new System.Text.RegularExpressions.Regex(@"(?<x>\d+),(?<y>\d+)");
@@ -53,6 +102,6 @@ Input GetInput(string fileName)
 
 readonly record struct Point(int x, int y);
 
-readonly record struct Input(List<Point> Points, List<Point> Folds);
+readonly record struct Input(HashSet<Point> Points, List<Point> Folds);
 
 
