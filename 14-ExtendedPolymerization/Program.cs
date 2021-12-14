b2c997ee3b0b810e30f26285615b6312ebe36e3a
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-14 19:25:54 +0100

    read input for 14-EP

--- 14-ExtendedPolymerization/Program.cs
+++ 14-ExtendedPolymerization/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = GetInput(@"input-small.txt");
+
+Input GetInput(string fileName)
+{
+  List<InsertionRule> rules = new List<InsertionRule>();
+  string? template = null;
+
+  var lines = File.ReadLines(fileName);
+  foreach (var line in lines)
+  {
+    var regexInsertion = new System.Text.RegularExpressions.Regex(@"(?<pair>[A-Z]{2}) -> (?<ins>[A-Z])");
+    var regexTemp = new System.Text.RegularExpressions.Regex(@"[A-Z]+");
+    var match = regexInsertion.Match(line);
+    if (match.Success)
+    {
+      var insertionRule = new InsertionRule(match.Groups["pair"].Value, match.Groups["ins"].Value);
+      rules.Add(insertionRule);
+    }
+    else
+    { 
+      var m = regexTemp.Match(line);
+      if (m.Success)
+      {
+        System.Diagnostics.Debug.Assert(template == null);
+        template = m.Value;
+      }
+      else
+      {
+        System.Diagnostics.Debug.Assert(string.IsNullOrEmpty(line));
+      }
+    }
+  }
+
+  System.Diagnostics.Debug.Assert(template != null);
+  return new Input(template, rules);
+}
+
+readonly record struct InsertionRule(string pair, string insert);
+readonly record struct Input(string PolymerTemplate, IEnumerable<InsertionRule> InsertionRules);
