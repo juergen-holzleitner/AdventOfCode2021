0f701bf2c50aa61379a4ac05c28163cf6e459122
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-15 16:27:08 +0100

    part2 of 15-Chiton

--- 15-Chiton/Program.cs
+++ 15-Chiton/Program.cs
@@ -1,5 +1,7 @@
 ﻿var input = GetInput(@"input.txt");
 
+input = GetExpandedBy(input, 5);
+
 var finalPos = new Pos(input.Count() - 1, input[input.Count() - 1].Count() - 1);
 var startPos = new Pos(0, 0);
 input[startPos.row][startPos.col].MinTotalCost = 0;
@@ -41,6 +43,53 @@ IEnumerable<Pos> GetNextSteps(Pos currentPos)
     yield return currentPos with { row = currentPos.row + 1 };
   if (currentPos.col < input[currentPos.row].Count() - 1)
     yield return currentPos with { col = currentPos.col + 1 };
+}
+
+List<List<Field>> GetExpandedBy(List<List<Field>> field, int expansion)
+{ 
+  var res = new List<List<Field>>();
+  for (int row = 0; row < field.Count; row++)
+  {
+    var r = new List<Field>();
+    for (int col = 0; col < field[row].Count * expansion; col++)
+    {
+      var newCost = field[row][col % field[row].Count].Cost;
+      newCost += col / field[row].Count;
+      newCost = CalcModTen(newCost);
+
+      r.Add(new Field(newCost, null));
+    }
+
+    res.Add(r);
+  }
+
+  for (int row = field.Count; row< field.Count*expansion; row++)
+  {
+    var baseRow = row % field.Count;
+    int additionalCose = row / field.Count;
+    var r = new List<Field>();
+
+    for (int col = 0; col < res[baseRow].Count; ++col)
+    {
+      var newCost = res[baseRow][col].Cost;
+      newCost += additionalCose;
+      newCost = CalcModTen(newCost);
+      r.Add(new Field(newCost, null));
+    }
+
+    res.Add(r);
+  }
+
+  return res;
+}
+
+int CalcModTen(int val)
+{
+  while (val >= 10)
+  {
+    val -= 9;
+  }
+  return val;
 }
 
 List<List<Field>> GetInput(string fileName)
