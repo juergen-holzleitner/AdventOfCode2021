bff30a7f04e5a141c3e2cee850d54ea39b8be19d
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-19 18:18:51 +0100

    RotationsXPos

--- 19-BeaconScanner/TestMatrix.cs
+++ 19-BeaconScanner/TestMatrix.cs
@@ -20,6 +20,24 @@ namespace _19_BeaconScanner
       Assert.AreEqual(beacon, res);
     }
 
+    [TestMethod]
+    public void RotationsXPos()
+    {
+      var beacon = new Beacon(0, 1, 2);
+      
+      var res = Matrix.GetRotationMatrices().ToList()[0].Multiply(beacon);
+      Assert.AreEqual(new Beacon(0, 1, 2), res);
+
+      res = Matrix.GetRotationMatrices().ToList()[1].Multiply(beacon);
+      Assert.AreEqual(new Beacon(0, -2, 1), res);
+
+      res = Matrix.GetRotationMatrices().ToList()[2].Multiply(beacon);
+      Assert.AreEqual(new Beacon(0, -1, -2), res);
+
+      res = Matrix.GetRotationMatrices().ToList()[3].Multiply(beacon);
+      Assert.AreEqual(new Beacon(0, 2, -1), res);
+    }
+
 
     class Matrix
     {
@@ -36,13 +54,52 @@ namespace _19_BeaconScanner
         };
       }
 
+      public static IEnumerable<Matrix> GetRotationMatrices()
+      {
+        // +X
+        yield return GetIdentity();
+
+        // +X 90 deg
+        yield return new Matrix()
+        {
+          mat = new int[3, 3]
+          {
+            { 1,  0, 0 },
+            { 0,  0, 1 },
+            { 0, -1, 0 },
+          }
+        };
+        
+        // +X 180 deg
+        yield return new Matrix()
+        {
+          mat = new int[3, 3]
+          {
+            { 1,  0,  0 },
+            { 0, -1,  0 },
+            { 0,  0, -1 },
+          }
+        };
+
+        // +X 270 deg
+        yield return new Matrix()
+        {
+          mat = new int[3, 3]
+          {
+            { 1,  0,  0 },
+            { 0,  0, -1 },
+            { 0,  1,  0 },
+          }
+        };
+      }
+
       internal Beacon Multiply(Beacon beacon)
       {
         System.Diagnostics.Debug.Assert(mat != null);
 
-        var X = beacon.X * mat[0, 0] + beacon.X * mat[1, 0] + beacon.X * mat[2, 0];
-        var Y = beacon.Y * mat[0, 1] + beacon.Y * mat[1, 1] + beacon.Y * mat[2, 1];
-        var Z = beacon.Z * mat[0, 2] + beacon.Z * mat[1, 2] + beacon.Z * mat[2, 2];
+        var X = beacon.X * mat[0, 0] + beacon.Y * mat[1, 0] + beacon.Z * mat[2, 0];
+        var Y = beacon.X * mat[0, 1] + beacon.Y * mat[1, 1] + beacon.Z * mat[2, 1];
+        var Z = beacon.X * mat[0, 2] + beacon.Y * mat[1, 2] + beacon.Z * mat[2, 2];
 
         return new Beacon(X, Y, Z);
       }
