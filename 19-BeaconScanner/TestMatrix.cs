916e793edbd5dcc61b60231e4771b6d342dad9d5
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-19 18:23:13 +0100

    add RotationsXNeg

--- 19-BeaconScanner/TestMatrix.cs
+++ 19-BeaconScanner/TestMatrix.cs
@@ -23,21 +23,38 @@ namespace _19_BeaconScanner
     [TestMethod]
     public void RotationsXPos()
     {
-      var beacon = new Beacon(0, 1, 2);
+      var beacon = new Beacon(3, 1, 2);
       
       var res = Matrix.GetRotationMatrices().ToList()[0].Multiply(beacon);
-      Assert.AreEqual(new Beacon(0, 1, 2), res);
+      Assert.AreEqual(new Beacon(3, 1, 2), res);
 
       res = Matrix.GetRotationMatrices().ToList()[1].Multiply(beacon);
-      Assert.AreEqual(new Beacon(0, -2, 1), res);
+      Assert.AreEqual(new Beacon(3, -2, 1), res);
 
       res = Matrix.GetRotationMatrices().ToList()[2].Multiply(beacon);
-      Assert.AreEqual(new Beacon(0, -1, -2), res);
+      Assert.AreEqual(new Beacon(3, -1, -2), res);
 
       res = Matrix.GetRotationMatrices().ToList()[3].Multiply(beacon);
-      Assert.AreEqual(new Beacon(0, 2, -1), res);
+      Assert.AreEqual(new Beacon(3, 2, -1), res);
     }
 
+    [TestMethod]
+    public void RotationsXNeg()
+    {
+      var beacon = new Beacon(3, 1, 2);
+
+      var res = Matrix.GetRotationMatrices().ToList()[4].Multiply(beacon);
+      Assert.AreEqual(new Beacon(-3, 1, 2), res);
+
+      res = Matrix.GetRotationMatrices().ToList()[5].Multiply(beacon);
+      Assert.AreEqual(new Beacon(-3, 2, -1), res);
+
+      res = Matrix.GetRotationMatrices().ToList()[6].Multiply(beacon);
+      Assert.AreEqual(new Beacon(-3, -1, -2), res);
+
+      res = Matrix.GetRotationMatrices().ToList()[7].Multiply(beacon);
+      Assert.AreEqual(new Beacon(-3, -2, 1), res);
+    }
 
     class Matrix
     {
@@ -89,6 +106,51 @@ namespace _19_BeaconScanner
             { 1,  0,  0 },
             { 0,  0, -1 },
             { 0,  1,  0 },
+          }
+        };
+
+
+        // -X
+        yield return new Matrix()
+        {
+          mat = new int[3, 3]
+          {
+            { -1,  0,  0 },
+            {  0,  1,  0 },
+            {  0,  0,  1 },
+          }
+        };
+
+        // -X 90 deg
+        yield return new Matrix()
+        {
+          mat = new int[3, 3]
+          {
+            { -1,  0,  0 },
+            {  0,  0, -1 },
+            {  0,  1,  0 },
+          }
+        };
+
+        // -X 180 deg
+        yield return new Matrix()
+        {
+          mat = new int[3, 3]
+          {
+            { -1,  0,  0 },
+            {  0, -1,  0 },
+            {  0,  0, -1 },
+          }
+        };
+
+        // -X 270 deg
+        yield return new Matrix()
+        {
+          mat = new int[3, 3]
+          {
+            { -1,  0, 0 },
+            {  0,  0, 1 },
+            {  0, -1, 0 },
           }
         };
       }
