ebf962f7faf8bda2b862ba38fb8888ec0d495783
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-19 21:47:23 +0100

    add TestSmallInput

---
+++ 19-BeaconScanner/TestSmallInput.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_BeaconScanner
+{
+  [TestClass]
+  public class TestSmallInput
+  {
+    [TestMethod]
+    public void CheckNumMatches()
+    {
+      var allScanner = Parser.ParseScanner(File.ReadLines(@"input-small.txt").GetEnumerator()).ToList();
+      Assert.AreEqual(5, allScanner.Count);
+
+    }
+  }
+}
