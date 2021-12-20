f94d720d35066e8164062ada879514e2e4f4afe0
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-20 22:52:21 +0100

    add 20-TrenchMap

---
+++ 20-TrenchMap/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _20_TrenchMap
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
