51e8dc22fb9b80b66bea9fec3f3890a870c13f6f
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-24 20:31:08 +0100

    ExpandImageBy

--- 20-TrenchMap/TestExpand.cs
+++ 20-TrenchMap/TestExpand.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _20_TrenchMap
 {
@@ -16,6 +13,48 @@ namespace _20_TrenchMap
       var line = "#..#.".ToList();
       var expandedLine = ExpandLineBy(line, 2);
       CollectionAssert.AreEqual("..#..#...".ToList(), expandedLine);
+    }
+
+    [TestMethod]
+    public void TextExpandImage()
+    {
+      List<List<char>> image = new()
+      {
+        new List<char> {'.'}
+      };
+
+      var expandedImage = ExpandImageBy(image, 1);
+
+      List<List<char>> expandedTestImage = new()
+      {
+        new List<char> { '.', '.', '.' },
+        new List<char> { '.', '.', '.' },
+        new List<char> { '.', '.', '.' },
+      };
+
+      Assert.AreEqual(expandedTestImage.Count, expandedImage.Count);
+      for (int i = 0; i < image.Count; i++)
+        CollectionAssert.AreEqual(expandedTestImage[i], expandedImage[i]);
+    }
+
+    private static List<List<char>> ExpandImageBy(List<List<char>> image, int size)
+    {
+      var initialLength = image.First().Count;
+      var expanded = new List<List<char>>();
+      var expandLine = new List<char>();
+      for (int i = 0; i < initialLength + 2 * size; i++)
+        expandLine.Add('.');
+
+      for (int i = 0; i < size; i++)
+        expanded.Add(expandLine.ToList());
+
+      foreach (var l in image)
+        expanded.Add(ExpandLineBy(l, size));
+
+      for (int i = 0; i < size; i++)
+        expanded.Add(expandLine.ToList());
+
+      return expanded;
     }
 
     private static List<char> ExpandLineBy(List<char> line, int size)
