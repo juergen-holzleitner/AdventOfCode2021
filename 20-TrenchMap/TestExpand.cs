6507b95d17be62662b13aad9fd892d60825bc82e
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2021-12-24 12:24:00 +0100

    add TestExpandLine

---
+++ 20-TrenchMap/TestExpand.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_TrenchMap
+{
+  [TestClass]
+  public class TestExpand
+  {
+    [TestMethod]
+    public void TestExpandLine()
+    {
+      string line = "#..#.";
+      string expandedLine = ExpandLineBy(line, 2);
+      Assert.AreEqual(".." + line + "..", expandedLine);
+    }
+
+    private string ExpandLineBy(string line, int size)
+    {
+      var expansion = new string('.', size);
+      return expansion + line + expansion;
+    }
+  }
+}
