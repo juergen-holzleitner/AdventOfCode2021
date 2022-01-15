39947b14ac89229efaf6224668ff3a4b7b488909
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-01-15 22:49:20 +0100

    Pawn

---
+++ 21-DiracDice/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _21_DiracDice
+{
+  internal class Pawn
+  {
+    public Pawn(int startPosition)
+    {
+      Position = startPosition;
+    }
+
+    public int Position { get; internal set; }
+
+    public int Score { get; internal set; } = 0;
+
+    internal void MoveTo(int finalPos)
+    {
+      Position = finalPos;
+      Score += Position;
+    }
+  }
+}
--- 21-DiracDice/TestGameBoard.cs
+++ 21-DiracDice/TestGameBoard.cs
@@ -14,5 +14,37 @@ namespace _21_DiracDice
       var newPos = GameBoard.IncrementPosBy(start, inc);
       Assert.AreEqual(end, newPos);
     }
+
+    [TestMethod]
+    public void PawnPosition_CanBeRequested()
+    {
+      var pawn = new Pawn(3);
+      Assert.AreEqual(3, pawn.Position);
+    }
+
+    [TestMethod]
+    public void Pawn_CanMove()
+    {
+      var pawn = new Pawn(3);
+      pawn.MoveTo(5);
+      Assert.AreEqual(5, pawn.Position);
+    }
+
+    [TestMethod]
+    public void PawnHasInitialScoreOfZero()
+    {
+      var pawn = new Pawn(3);
+      Assert.AreEqual(0, pawn.Score);
+    }
+
+    [TestMethod]
+    public void PawnScoreIncreasesByTargetPosition()
+    {
+      var pawn = new Pawn(3);
+      pawn.MoveTo(5);
+      Assert.AreEqual(5, pawn.Score);
+      pawn.MoveTo(7);
+      Assert.AreEqual(12, pawn.Score);
+    }
   }
 }
