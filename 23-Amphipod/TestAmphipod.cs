20915e0e23447dc0176b9c02aa11eba171f0883a
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-01-29 17:27:26 +0100

    add 23-Amphipid

---
+++ 23-Amphipod/TestAmphipod.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _23_Amphipod
+{
+  [TestClass]
+  public class TestAmphipod
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
