54f67a3a42c8203f531d31fd7d35e566a82ca647
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-01-29 17:52:50 +0100

    add Hallway

---
+++ 23-Amphipod/Hallway.cs
@@ -0,0 +1,12 @@
+﻿namespace _23_Amphipod
+{
+  internal class Hallway
+  {
+    public Hallway()
+    {
+    }
+
+    public int MinPosition { get => 0; }
+    public int MaxPosition { get => 10; }
+  }
+}
--- 23-Amphipod/TestAmphipod.cs
+++ 23-Amphipod/TestAmphipod.cs
@@ -6,8 +6,11 @@ namespace _23_Amphipod
   public class TestAmphipod
   {
     [TestMethod]
-    public void TestMethod1()
+    public void HallwayHasElevenPositions()
     {
+      var hallway = new Hallway();
+      Assert.AreEqual(10, hallway.MaxPosition);
+      Assert.AreEqual(0, hallway.MinPosition);
     }
   }
 }
