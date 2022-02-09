f375ce160902d7754ec138a3f73b34ee68b19dca
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-02-09 20:48:20 +0100

    add 24 ALU

---
+++ 24-ALU/TestParser.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _24_ALU
+{
+  [TestClass]
+  public class TestParser
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
