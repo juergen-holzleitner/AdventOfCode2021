f8e792822dd8245bc1a82500a2235ed6730c5ba0
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-02-10 22:02:08 +0100

    parse register

--- 24-ALU/Parser.cs
+++ 24-ALU/Parser.cs
@@ -5,6 +5,7 @@ namespace _24_ALU
   public class Parser
   {
     public enum Operation { inp, add, mul, div, mod, eql };
+    public enum Register { w, x, y, z}
 
     public Parser()
     {
@@ -13,18 +14,39 @@ namespace _24_ALU
     public Instruction ParseLine(string codeLine)
     {
       var elements = codeLine.Split(' ');
-      switch (elements[0])
-      {
-        case "inp": return new(Operation.inp);
-        case "add": return new(Operation.add);
-        case "mul": return new(Operation.mul);
-        case "div": return new(Operation.div);
-        case "mod": return new(Operation.mod);
-        case "eql": return new(Operation.eql);
-      }
-      throw new ArgumentException("invalid instruction", nameof(codeLine));
+
+      var operation = ParseOperation(elements[0]);
+      var register = ParseRegister(elements[1]);
+      return new(operation, register);
     }
 
-    public record Instruction(Operation Operation);
+    private Register ParseRegister(string register)
+    {
+      switch (register)
+      {
+        case "w": return Register.w;
+        case "x": return Register.x;
+        case "y": return Register.y;
+        case "z": return Register.z;
+      }
+
+      throw new ArgumentException("invalid register", nameof(register));
+    }
+
+    private Operation ParseOperation(string operation)
+    {
+      switch (operation)
+      {
+        case "inp": return Operation.inp;
+        case "add": return Operation.add;
+        case "mul": return Operation.mul;
+        case "div": return Operation.div;
+        case "mod": return Operation.mod;
+        case "eql": return Operation.eql;
+      }
+      throw new ArgumentException("invalid operation", nameof(operation));
+    }
+
+    public record Instruction(Operation Operation, Register Register);
   }
 }
--- 24-ALU/TestParser.cs
+++ 24-ALU/TestParser.cs
@@ -7,12 +7,12 @@ namespace _24_ALU
   public class TestParser
   {
     [Theory]
-    [InlineData("inp a", Parser.Operation.inp)]
-    [InlineData("add a b", Parser.Operation.add)]
-    [InlineData("mul a b", Parser.Operation.mul)]
-    [InlineData("div a b", Parser.Operation.div)]
-    [InlineData("mod a b", Parser.Operation.mod)]
-    [InlineData("eql a b", Parser.Operation.eql)]
+    [InlineData("inp x", Parser.Operation.inp)]
+    [InlineData("add x y", Parser.Operation.add)]
+    [InlineData("mul x y", Parser.Operation.mul)]
+    [InlineData("div x y", Parser.Operation.div)]
+    [InlineData("mod x y", Parser.Operation.mod)]
+    [InlineData("eql x y", Parser.Operation.eql)]
     public void Can_read_instruction(string code, Parser.Operation operation)
     {
       var sut = new Parser();
@@ -26,11 +26,37 @@ namespace _24_ALU
     public void Invalid_instruction_throws()
     {
       var sut = new Parser();
-      string code = "asdf";
+      string code = "asdf x";
 
       var act = () => sut.ParseLine(code);
 
       act.Should().Throw<ArgumentException>();
     }
+
+    [Theory]
+    [InlineData("inp x", Parser.Register.x)]
+    [InlineData("inp y", Parser.Register.y)]
+    [InlineData("inp z", Parser.Register.z)]
+    [InlineData("inp w", Parser.Register.w)]
+    public void Can_Parse_Register(string code, Parser.Register expectedRegister)
+    {
+      var sut = new Parser();
+
+      var instruction = sut.ParseLine(code);
+
+      instruction.Register.Should().Be(expectedRegister);
+    }
+
+    [Fact]
+    public void Invalid_register_throws()
+    {
+      var sut = new Parser();
+      string code = "inp a";
+
+      var act = () => sut.ParseLine(code);
+
+      act.Should().Throw<ArgumentException>();
+    }
+
   }
 }
