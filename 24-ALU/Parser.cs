80fc9c61794d66316ebc4a3795d6f63e80b1fc53
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-02-10 21:32:48 +0100

    instruction parsing

--- 24-ALU/Parser.cs
+++ 24-ALU/Parser.cs
@@ -4,7 +4,7 @@ namespace _24_ALU
 {
   public class Parser
   {
-    public enum Operation { inp, add };
+    public enum Operation { inp, add, mul, div, mod, eql };
 
     public Parser()
     {
@@ -12,7 +12,17 @@ namespace _24_ALU
 
     public Instruction ParseLine(string codeLine)
     {
-      return new(Operation.inp);
+      var elements = codeLine.Split(' ');
+      switch (elements[0])
+      {
+        case "inp": return new(Operation.inp);
+        case "add": return new(Operation.add);
+        case "mul": return new(Operation.mul);
+        case "div": return new(Operation.div);
+        case "mod": return new(Operation.mod);
+        case "eql": return new(Operation.eql);
+      }
+      throw new ArgumentException("invalid instruction", nameof(codeLine));
     }
 
     public record Instruction(Operation Operation);
--- 24-ALU/TestParser.cs
+++ 24-ALU/TestParser.cs
@@ -1,4 +1,6 @@
 using Xunit;
+using FluentAssertions;
+using System;
 
 namespace _24_ALU
 {
@@ -7,13 +9,28 @@ namespace _24_ALU
     [Theory]
     [InlineData("inp a", Parser.Operation.inp)]
     [InlineData("add a b", Parser.Operation.add)]
+    [InlineData("mul a b", Parser.Operation.mul)]
+    [InlineData("div a b", Parser.Operation.div)]
+    [InlineData("mod a b", Parser.Operation.mod)]
+    [InlineData("eql a b", Parser.Operation.eql)]
     public void Can_read_instruction(string code, Parser.Operation operation)
     {
       var sut = new Parser();
 
       var instruction = sut.ParseLine(code);
 
-      Assert.AreEqual(operation, instruction.Operation);
+      instruction.Operation.Should().Be(operation);
+    }
+
+    [Fact]
+    public void Invalid_instruction_throws()
+    {
+      var sut = new Parser();
+      string code = "asdf";
+
+      var act = () => sut.ParseLine(code);
+
+      act.Should().Throw<ArgumentException>();
     }
   }
 }
