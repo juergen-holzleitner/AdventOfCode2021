ed441533a166aca3cbf59bca94f9b037d7a4eff8
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-02-13 20:43:16 +0100

    add ALU

---
+++ 24-ALU/ALU.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _24_ALU
+{
+  internal class ALU
+  {
+    readonly Dictionary<Parser.Register, int> registerValue = new();
+    readonly IEnumerator<int>? inputs = null;
+
+    public ALU()
+    {
+      foreach (var reg in Enum.GetValues(typeof(Parser.Register)))
+        registerValue.Add((Parser.Register)reg, 0);
+    }
+
+    public ALU(IEnumerable<int> inputs)
+      : this()
+    {
+      this.inputs = inputs.GetEnumerator();
+    }
+
+    internal int GetValue(Parser.Register register)
+    {
+      return registerValue[register];
+    }
+
+    internal void ProcessInstrution(Parser.Instruction instruction)
+    {
+      switch (instruction.Operation)
+      {
+        case Parser.Operation.add:
+          registerValue[instruction.Register] += GetOperandValue(instruction.Operand);
+          break;
+        case Parser.Operation.mul:
+          registerValue[instruction.Register] *= GetOperandValue(instruction.Operand);
+          break;
+        case Parser.Operation.div:
+          registerValue[instruction.Register] /= GetOperandValue(instruction.Operand);
+          break;
+        case Parser.Operation.mod:
+          var oldVal = registerValue[instruction.Register];
+          if (oldVal < 0)
+            throw new InvalidOperationException("Module with negative register value");
+          var operandVal = GetOperandValue(instruction.Operand);
+          if (operandVal < 0)
+            throw new InvalidOperationException("Module with negative operand value");
+          registerValue[instruction.Register] = oldVal % operandVal;
+          break;
+        case Parser.Operation.inp:
+          if (inputs != null && inputs.MoveNext())
+          {
+            registerValue[instruction.Register] = inputs.Current;
+            break;
+          }
+          throw new InvalidOperationException();
+        case Parser.Operation.eql:
+          registerValue[instruction.Register] = registerValue[instruction.Register] == GetOperandValue(instruction.Operand) ? 1 : 0;
+          break;
+      }
+    }
+
+    private int GetOperandValue(Parser.IOperand? operand)
+    {
+      if (operand is Parser.RegisterOperand register)
+        return registerValue[register.Register];
+      else if (operand is Parser.NumberOperand number)
+        return number.Number;
+      else
+        throw new ArgumentException("Invalid operand argument", nameof(operand));
+    }
+  }
+}
---
+++ 24-ALU/TestALU.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using static _24_ALU.Parser;
+
+namespace _24_ALU
+{
+  public class TestALU
+  {
+    [Theory]
+    [InlineData(Register.w)]
+    [InlineData(Register.x)]
+    [InlineData(Register.y)]
+    [InlineData(Register.z)]
+    public void All_ALU_registers_are_zero_initially(Register register)
+    {
+      var sut = new ALU();
+
+      var val = sut.GetValue(register);
+
+      val.Should().Be(0);
+    }
+
+    [Fact]
+    public void Add_increases_Register()
+    {
+      var sut = new ALU();
+      const int TestNumber = 5;
+      var instruction = new Instruction(Operation.add, Register.x, new NumberOperand(TestNumber));
+
+      sut.ProcessInstrution(instruction);
+
+      var val = sut.GetValue(Register.x);
+      val.Should().Be(TestNumber);
+    }
+
+    [Fact]
+    public void Mul_multiplies_Register()
+    {
+      var sut = new ALU();
+      const int TestNumber = 5;
+      var addX = new Instruction(Operation.add, Register.x, new NumberOperand(TestNumber));
+      var addY = new Instruction(Operation.add, Register.y, new NumberOperand(TestNumber));
+      var mul = new Instruction(Operation.mul, Register.y, new RegisterOperand(Register.x));
+      sut.ProcessInstrution(addX);
+      sut.ProcessInstrution(addY);
+
+      sut.ProcessInstrution(mul);
+
+      var val = sut.GetValue(Register.y);
+      val.Should().Be(25);
+    }
+
+    [Fact]
+    public void Div_truncates_value()
+    {
+      var sut = new ALU();
+      var addW = new Instruction(Operation.add, Register.w, new NumberOperand(-7));
+      var div = new Instruction(Operation.div, Register.w, new NumberOperand(3));
+      sut.ProcessInstrution(addW);
+
+      sut.ProcessInstrution(div);
+
+      var val = sut.GetValue(Register.w);
+      val.Should().Be(-2);
+    }
+
+    [Fact]
+    public void DivByZero_is_handled()
+    {
+      var sut = new ALU();
+      var addW = new Instruction(Operation.add, Register.w, new NumberOperand(-7));
+      var div = new Instruction(Operation.div, Register.w, new NumberOperand(0));
+      sut.ProcessInstrution(addW);
+
+      var act = () => sut.ProcessInstrution(div);
+
+      act.Should().Throw<DivideByZeroException>();
+    }
+
+    [Fact]
+    public void Mod_works_for_positive_value()
+    {
+      var sut = new ALU();
+      var addX = new Instruction(Operation.add, Register.x, new NumberOperand(10));
+      var mod = new Instruction(Operation.mod, Register.x, new NumberOperand(4));
+
+      sut.ProcessInstrution(addX);
+      sut.ProcessInstrution(mod);
+
+      var val = sut.GetValue(Register.x);
+      val.Should().Be(2);
+    }
+
+    [Fact]
+    public void Mod_throws_if_register_is_negative()
+    {
+      var sut = new ALU();
+      var addX = new Instruction(Operation.add, Register.x, new NumberOperand(-1));
+      var mod = new Instruction(Operation.mod, Register.x, new NumberOperand(1));
+
+      sut.ProcessInstrution(addX);
+      var act = () => sut.ProcessInstrution(mod);
+
+      act.Should().Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void Mod_throws_if_operand_is_invalid()
+    {
+      var sut = new ALU();
+      var addX = new Instruction(Operation.add, Register.x, new NumberOperand(1));
+      var mod = new Instruction(Operation.mod, Register.x, new NumberOperand(-2));
+
+      sut.ProcessInstrution(addX);
+      var act = () => sut.ProcessInstrution(mod);
+
+      act.Should().Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void ModByZero_is_handled()
+    {
+      var sut = new ALU();
+      var addX = new Instruction(Operation.add, Register.x, new NumberOperand(1));
+      var mod = new Instruction(Operation.mod, Register.x, new NumberOperand(0));
+
+      sut.ProcessInstrution(addX);
+      var act = () => sut.ProcessInstrution(mod);
+
+      act.Should().Throw<DivideByZeroException>();
+    }
+
+    [Fact]
+    public void ReadInputValue_throws_on_empty_inputstream()
+    {
+      var sut = new ALU();
+      var inpX = new Instruction(Operation.inp, Register.x, null);
+
+      var act = () => sut.ProcessInstrution(inpX);
+
+      act.Should().Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void ReadInput_stores_value_in_register()
+    {
+      var inputs = new List<int> { 5 };
+      var sut = new ALU(inputs);
+      var inpX = new Instruction(Operation.inp, Register.x, null);
+
+      sut.ProcessInstrution(inpX);
+
+      var val = sut.GetValue(Register.x);
+      val.Should().Be(5);
+    }
+
+    [Fact]
+    public void Equal_is_one_if_matches()
+    {
+      var sut = new ALU();
+      var addX = new Instruction(Operation.add, Register.x, new NumberOperand(5));
+      var equ = new Instruction(Operation.eql, Register.x, new NumberOperand(5));
+      sut.ProcessInstrution(addX);
+
+      sut.ProcessInstrution(equ);
+
+      var val = sut.GetValue(Register.x);
+      val.Should().Be(1);
+    }
+
+    [Fact]
+    public void Equal_is_zero_if_nomatch()
+    {
+      var sut = new ALU();
+      var addX = new Instruction(Operation.add, Register.x, new NumberOperand(5));
+      var equ = new Instruction(Operation.eql, Register.x, new NumberOperand(6));
+      sut.ProcessInstrution(addX);
+
+      sut.ProcessInstrution(equ);
+
+      var val = sut.GetValue(Register.x);
+      val.Should().Be(0);
+    }
+
+  }
+}
