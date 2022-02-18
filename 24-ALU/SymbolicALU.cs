b8c58ff463b851bc6114a541051acb38ab8713ff
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-02-18 23:09:39 +0100

    add some more basic operations

--- 24-ALU/SymbolicALU.cs
+++ 24-ALU/SymbolicALU.cs
@@ -24,26 +24,97 @@ namespace _24_ALU
     {
       if(instruction.Operation == Operation.inp)
       {
-        register[instruction.Register] = new InputOperand(inputIndex);
-        ++inputIndex;
+        ProcessInp(instruction.Register);
         return;
       }
-      else if (instruction.Operation == Operation.add)
+      else if (instruction.Operation == Operation.add && instruction.Operand is not null)
       {
-        if (instruction.Operand is NumberOperand num2)
-        {
-          if (num2.Number == 0)
-            return;
+        ProcessAdd(instruction.Register, instruction.Operand);
+        return;
+      }
+      else if (instruction.Operation == Operation.mul && instruction.Operand is not null)
+      {
+        ProcessMul(instruction.Register, instruction.Operand);
+        return;
+      }
+      else if (instruction.Operation == Operation.div && instruction.Operand is not null)
+      {
+        ProcessDiv(instruction.Register, instruction.Operand);
+        return;
+      }
+      else if (instruction.Operation == Operation.mod && instruction.Operand is not null)
+      {
+        ProcessMod(instruction.Register, instruction.Operand);
+        return;
+      }
 
-          if (register[instruction.Register] is NumberOperand num1)
-          {
-            register[instruction.Register] = new NumberOperand(num1.Number + num2.Number);
-            return;
-          }
+      throw new NotImplementedException();
+    }
+
+    private void ProcessMod(Register reg, IOperand operand)
+    {
+      if (operand is NumberOperand num2)
+      {
+        if (num2.Number == 1)
+        {
+          register[reg] = new NumberOperand(0);
+          return;
         }
       }
 
       throw new NotImplementedException();
+    }
+
+    private void ProcessDiv(Register reg, IOperand operand)
+    {
+      if (operand is NumberOperand num2)
+      {
+        if (num2.Number == 1)
+          return;
+      }
+
+      throw new NotImplementedException();
+    }
+
+    private void ProcessMul(Register reg, IOperand operand)
+    {
+      if (operand is NumberOperand num2)
+      {
+        if (num2.Number == 0)
+        {
+          register[reg] = new NumberOperand(0);
+          return;
+        }
+        else if (num2.Number == 1)
+        {
+          return;
+        }
+      }
+
+      throw new NotImplementedException();
+    }
+
+    private void ProcessAdd(Register reg, IOperand operand)
+    {
+      if (operand is NumberOperand num2)
+      {
+        if (num2.Number == 0)
+          return;
+
+        if (register[reg] is NumberOperand num1)
+        {
+          register[reg] = new NumberOperand(num1.Number + num2.Number);
+          return;
+        }
+      }
+
+      throw new NotImplementedException();
+    }
+
+    private void ProcessInp(Register reg)
+    {
+      register[reg] = new InputOperand(inputIndex);
+      ++inputIndex;
     }
   }
 }
--- 24-ALU/TestSymbolicALU.cs
+++ 24-ALU/TestSymbolicALU.cs
@@ -25,9 +25,9 @@ namespace _24_ALU
     {
       var sut = new SymbolicALU();
       const int testNumber = 5;
-      var instruction = new Instruction(Operation.add, Register.x, new NumberOperand(testNumber));
+      var add = new Instruction(Operation.add, Register.x, new NumberOperand(testNumber));
 
-      sut.ProcessInstruction(instruction);
+      sut.ProcessInstruction(add);
 
       var value = sut.GetValue(Register.x);
       value.As<NumberOperand>().Number.Should().Be(testNumber);
@@ -37,9 +37,9 @@ namespace _24_ALU
     public void Inp_returns_Inp_Instruction()
     {
       var sut = new SymbolicALU();
-      var instruction = new Instruction(Operation.inp, Register.x, null);
+      var inp = new Instruction(Operation.inp, Register.x, null);
 
-      sut.ProcessInstruction(instruction);
+      sut.ProcessInstruction(inp);
 
       var value = sut.GetValue(Register.x);
       value.As<InputOperand>().Index.Should().Be(0);
@@ -49,16 +49,72 @@ namespace _24_ALU
     public void Inp_MultipleTimes_returns_correct_input_index()
     {
       var sut = new SymbolicALU();
-      var instruction = new Instruction(Operation.inp, Register.w, null);
+      var inp = new Instruction(Operation.inp, Register.w, null);
       
       const int numInputs = 5;
       for (int n = 0; n < numInputs + 1; ++n)
       {
-        sut.ProcessInstruction(instruction);
+        sut.ProcessInstruction(inp);
       }
 
       var value = sut.GetValue(Register.w);
       value.As<InputOperand>().Index.Should().Be(numInputs);
+    }
+
+    [Fact]
+    public void Mul_with_zero_yields_zero()
+    {
+      var sut = new SymbolicALU();
+      var inp = new Instruction(Operation.inp, Register.x, null);
+      sut.ProcessInstruction(inp);
+      var mul = new Instruction(Operation.mul, Register.x, new NumberOperand(0));
+
+      sut.ProcessInstruction(mul);
+
+      var value = sut.GetValue(Register.x);
+      value.As<NumberOperand>().Number.Should().Be(0);
+    }
+
+    [Fact]
+    public void Div_by_one_does_not_change()
+    {
+      var sut = new SymbolicALU();
+      var inp = new Instruction(Operation.inp, Register.x, null);
+      sut.ProcessInstruction(inp);
+      var div = new Instruction(Operation.div, Register.x, new NumberOperand(1));
+
+      sut.ProcessInstruction(div);
+
+      var value = sut.GetValue(Register.x);
+      value.As<InputOperand>().Index.Should().Be(0);
+    }
+
+    [Fact]
+    public void Mul_by_one_does_not_change()
+    {
+      var sut = new SymbolicALU();
+      var inp = new Instruction(Operation.inp, Register.x, null);
+      sut.ProcessInstruction(inp);
+      var mul = new Instruction(Operation.mul, Register.x, new NumberOperand(1));
+
+      sut.ProcessInstruction(mul);
+
+      var value = sut.GetValue(Register.x);
+      value.As<InputOperand>().Index.Should().Be(0);
+    }
+
+    [Fact]
+    public void Mod_by_one_is_zero()
+    {
+      var sut = new SymbolicALU();
+      var inp = new Instruction(Operation.inp, Register.x, null);
+      sut.ProcessInstruction(inp);
+      var mod = new Instruction(Operation.mod, Register.x, new NumberOperand(1));
+
+      sut.ProcessInstruction(mod);
+
+      var value = sut.GetValue(Register.x);
+      value.As<NumberOperand>().Number.Should().Be(0);
     }
   }
 }
