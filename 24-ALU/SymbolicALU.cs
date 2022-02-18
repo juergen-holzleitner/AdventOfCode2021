1122c03245166cb54cc87f1057385bfc3523ace1
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-02-18 22:20:59 +0100

    add SymbolicALU

---
+++ 24-ALU/SymbolicALU.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using static _24_ALU.Parser;
+
+namespace _24_ALU
+{
+  internal class SymbolicALU
+  {
+    readonly Dictionary<Register, IOperand> register = new();
+
+    public SymbolicALU()
+    {
+      foreach (var reg in Enum.GetValues(typeof(Register)))
+        register.Add((Register)reg, new NumberOperand(0));
+    }
+
+    internal IOperand GetValue(Register register)
+    {
+      return this.register[register];
+    }
+
+    internal void ProcessInstruction(Instruction instruction)
+    {
+      if (instruction.Operation == Operation.add)
+      {
+        if (instruction.Operand is NumberOperand num2)
+        {
+          if (num2.Number == 0)
+            return;
+
+          if (register[instruction.Register] is NumberOperand num1)
+          {
+            register[instruction.Register] = new NumberOperand(num1.Number + num2.Number);
+            return;
+          }
+        }
+      }
+
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ 24-ALU/TestSymbolicALU.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using FluentAssertions;
+using static _24_ALU.Parser;
+
+namespace _24_ALU
+{
+  public class TestSymbolicALU
+  {
+    [Theory]
+    [InlineData(Register.w)]
+    [InlineData(Register.x)]
+    [InlineData(Register.y)]
+    [InlineData(Register.z)]
+    public void SymbolicALU_is_initially_zero(Register register)
+    {
+      var sut = new SymbolicALU();
+
+      var value = sut.GetValue(register);
+
+      value.As<NumberOperand>().Number.Should().Be(0);
+    }
+
+    [Fact]
+    public void Add_const_value_returns_value()
+    {
+      var sut = new SymbolicALU();
+      const int testNumber = 5;
+      var instruction = new Instruction(Operation.add, Register.x, new NumberOperand(testNumber));
+
+      sut.ProcessInstruction(instruction);
+
+      var value = sut.GetValue(Register.x);
+      value.As<NumberOperand>().Number.Should().Be(testNumber);
+    }
+  }
+}
