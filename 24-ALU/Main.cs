836af5482b76112da7a53308bf1cecd4d1dac085
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-02-19 22:58:42 +0100

    print all valid numbers

--- 24-ALU/Main.cs
+++ 24-ALU/Main.cs
@@ -11,6 +11,7 @@ using System.Text;
 // run old algorithm with random inputs to find a valid number
 
 SymbolicAnalysis();
+CalcFinalModelNumber();
 
 void SymbolicAnalysis()
 {
@@ -28,10 +29,10 @@ void SymbolicAnalysis()
     var instruction = Parser.ParseLine(line);
     alu.ProcessInstruction(instruction);
 
-    System.Console.WriteLine($"{lineNumber}: {line}");
+    //System.Console.WriteLine($"{lineNumber}: {line}");
 
-    System.Console.WriteLine(Parser.FormatSymbolicALU(alu));
-    System.Console.ReadKey();
+    //System.Console.WriteLine(Parser.FormatSymbolicALU(alu));
+    //System.Console.ReadKey();
   }
 
   System.Console.WriteLine(Parser.FormatSymbolicALU(alu));
@@ -75,4 +76,65 @@ string FormatModelNumber(List<int> modelNumber)
   foreach (var n in modelNumber)
     sb.Append(n);
   return sb.ToString();
+}
+
+void CalcFinalModelNumber()
+{
+  for (long n0 = 9; n0 > 0; --n0)
+    for (long n1 = 9; n1 > 0; --n1)
+      for (long n2 = 9; n2 > 0; --n2)
+      {
+        long n3 = (((n0 + 13) * 26 + n1 + 10) * 26 + n2 + 3) % 26 + -11;
+        if (n3 <= 0 || n3 >= 10)
+          continue;
+
+        for (long n4 = 9; n4 > 0; --n4)
+        {
+          long n5 = ((((n0 + 13) * 26 + n1 + 10) * 26 + n2 + 3) / 26 * 26 + n4 + 9) % 26 + -4;
+          if (n5 <= 0 || n5 >= 10)
+            continue;
+
+          for (long n6 = 9; n6 > 0; --n6)
+            for (long n7 = 9; n7 > 0; --n7)
+              for (long n8 = 9; n8 > 0; --n8)
+              {
+                long n9 = (((((((n0 + 13) * 26 + n1 + 10) * 26 + n2 + 3) / 26 * 26 + n4 + 9) / 26 * 26 + n6 + 5) * 26 + n7 + 1) * 26 + n8) % 26 + -2;
+                if (n9 <= 0 || n9 >= 10)
+                  continue;
+
+                long n10 = ((((((((n0 + 13) * 26 + n1 + 10) * 26 + n2 + 3) / 26 * 26 + n4 + 9) / 26 * 26 + n6 + 5) * 26 + n7 + 1) * 26 + n8) / 26) % 26 + -5;
+                if (n10 <= 0 || n10 >= 10)
+                  continue;
+
+                long n11 = (((((((((n0 + 13) * 26 + n1 + 10) * 26 + n2 + 3) / 26 * 26 + n4 + 9) / 26 * 26 + n6 + 5) * 26 + n7 + 1) * 26 + n8) / 26) / 26) % 26 + -11;
+                if (n11 <= 0 || n11 >= 10)
+                  continue;
+
+                long n12 = ((((((((((n0 + 13) * 26 + n1 + 10) * 26 + n2 + 3) / 26 * 26 + n4 + 9) / 26 * 26 + n6 + 5) * 26 + n7 + 1) * 26 + n8) / 26) / 26) / 26) % 26 + -13;
+                if (n12 <= 0 || n12 >= 10)
+                  continue;
+
+                long n13 = ((((((((((n0 + 13) * 26 + n1 + 10) * 26 + n2 + 3) / 26 * 26 + n4 + 9) / 26 * 26 + n6 + 5) * 26 + n7 + 1) * 26 + n8) / 26) / 26) / 26) / 26 + -10;
+                if (n13 <= 0 || n13 >= 10)
+                  continue;
+
+                System.Console.Write("Found Number: ");
+                System.Console.Write(n0);
+                System.Console.Write(n1);
+                System.Console.Write(n2);
+                System.Console.Write(n3);
+                System.Console.Write(n4);
+                System.Console.Write(n5);
+                System.Console.Write(n6);
+                System.Console.Write(n7);
+                System.Console.Write(n8);
+                System.Console.Write(n9);
+                System.Console.Write(n10);
+                System.Console.Write(n11);
+                System.Console.Write(n12);
+                System.Console.Write(n13);
+                System.Console.WriteLine();
+              }
+        }
+      }
 }
