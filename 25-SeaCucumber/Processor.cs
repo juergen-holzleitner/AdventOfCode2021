574257fdfec2d50986dbd3fb45ffbfaa63d40d5f
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-03-06 12:17:29 +0100

    horizontal movement

--- 25-SeaCucumber/Processor.cs
+++ 25-SeaCucumber/Processor.cs
@@ -6,7 +6,22 @@ namespace _25_SeaCucumber
   {
     internal static char[] MoveHorizontal(char[] current)
     {
-      return new[] { '.', '>' };
+      var pos = new char[current.Length];
+      for (int i = 0; i < current.Length; i++)
+        pos[i] = '.';
+
+      for (int i = 0; i < current.Length; i++)
+      {
+        if (current[i] == '>')
+        {
+          var nextPos = (i + 1) % current.Length;
+          if (current[nextPos] == '.')
+            pos[nextPos] = '>';
+          else
+            pos[i] = '>';
+        }
+      }
+      return pos;
     }
   }
 }
--- 25-SeaCucumber/TestMovement.cs
+++ 25-SeaCucumber/TestMovement.cs
@@ -5,14 +5,19 @@ namespace _25_SeaCucumber
 {
   public class TestMovement
   {
-    [Fact]
-    public void MoveForward()
+    [Theory]
+    [InlineData(">.", ".>")]
+    [InlineData(".>", ">.")]
+    [InlineData("...>>>>>...", "...>>>>.>..")]
+    [InlineData("...>>>>.>..", "...>>>.>.>.")]
+    public void MoveHorizotal(string start, string expected)
     {
-      var initial = new[] { '>', '.' };
+      var initial = start.ToCharArray();
 
       var final = Processor.MoveHorizontal(initial);
 
-      final.Should().BeEquivalentTo(new[] { '.', '>' });
+      final.Should().Equal(expected.ToCharArray());
     }
+
   }
 }
